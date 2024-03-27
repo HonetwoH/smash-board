7e373c75b290206d059301f1142755cea81982cf
H12H <sucodetr@protonmail.com>
2024-03-27 20:22:58 +0530

    need to fix top_id first and also compute index
    push and fetch are working fine

--- src/main.rs
+++ src/main.rs
@@ -21,61 +21,79 @@ struct Db {
 type Blob = String;
 
 const DB_PATH: &str = "/tmp/smash.db";
+const LIMIT: usize = 8;
 
 impl Db {
-    fn create_db() -> Result<()> {
+    fn new() -> Result<Self> {
         let path = Path::new(DB_PATH);
-        if !path.exists() {
+        let conn = if !path.exists() {
             let conn = Connection::open(path)?;
             conn.execute(
                 "CREATE TABLE pastes (
-            id   INTEGER PRIMARY KEY,
-            data BLOB
-        )",
+                    id    INTEGER PRIMARY KEY,
+                    paste BLOB
+                )",
                 (), // empty list of parameters.
             )?;
-        }
-        Ok(())
+            conn
+        } else {
+            let conn = Connection::open(path)?;
+            conn
+        };
+        // let top_id = conn.query("SELECT id FROM pastes ORDER BY id DESC LIMIT 1", [])?;
+        Ok(Self { conn, top_id: 0 })
     }
 
     // compute index by respecting the constraints imposed
     // i.e is octal or hexadecimal
     // TODO: Make sure that there is the db does not change right underneath the function call
     fn compute_index(&self, idx: usize) -> usize {
-        self.top_id
+        //TODO: This method should return error along with the top_id
+        if idx <= LIMIT {
+            self.top_id - idx
+        } else {
+            self.top_id
+        }
     }
 
     // push is expected to work on single blob at a time hence the string directly
     fn push(&self, blob: Blob) -> Result<()> {
         self.conn
-            .execute("INSERT INTO pastes (data) VALUES (?1)", params![blob])?;
+            .execute("INSERT INTO pastes (paste) VALUES (?1)", params![blob])?;
         Ok(())
     }
 
     // fetch is expected to work with batch of blob indices
-    fn fetch(&self, blob_idxs: Vec<usize>) {
-        self.conn.execute_batch()
+    fn fetch(&self, blob_idxs: Vec<usize>) -> Vec<Result<Blob>> {
+        let mut query = self
+            .conn
+            .prepare("SELECT paste FROM pastes WHERE id = ?1")
+            .unwrap();
+        blob_idxs
+            .into_iter()
+            // .map(|x| self.compute_index(x))
+            .map(|x| {
+                // TODO: make sure that error from this part dont get unnoticed
+                query.query_row([x], |row| row.get(0))
+            })
+            .collect()
+    }
+
+    fn show(&self) {
+        dbg!(self.conn.execute("SELECT id, paste FROM pastes;", []));
     }
 }
-fn main() -> Result<()> {
-    let path = Path::new(DB_PATH);
-    let conn = Connection::open(path)?;
-    for i in 0..11 {
-        let me = Paste {
-            data: Some("This as message".to_string()),
-        };
-        conn.execute("INSERT INTO person (data) VALUES (?1)", params![&me.data])?;
-    }
-    let mut stmt = conn.prepare("SELECT id, data FROM person")?;
-    let out_iter = stmt.query_map([], |row| {
-        Ok(Out {
-            id: row.get(0)?,
-            data: row.get(1)?,
-        })
-    })?;
-
-    for person in out_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
-    Ok(())
+fn main() {
+    let db = Db::new().unwrap();
+    let _ = dbg!(db.push(String::from("Hello World")));
+    let _ = dbg!(db.push(String::from("Hello Mars")));
+    let _ = dbg!(db.push(String::from("Hello Mars")));
+    let _ = dbg!(db.push(String::from("Hello Venus")));
+    let _ = dbg!(db.push(String::from("Hello Jupiter")));
+    let _ = dbg!(db.push(String::from("Hello Neptune")));
+    let _ = dbg!(db.push(String::from("Hello Mercury")));
+    let _ = dbg!(db.push(String::from("Hello Uranas")));
+    dbg!(db.fetch(vec![1, 2, 3]));
+    // dbg!(db.conn.prepare());
+    // .execute("SELECT id FROM pastes ORDER BY id DESC LIMIT 1", []));
 }
