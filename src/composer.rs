e88f0c2820591e7e394759e8eb092c8500712ee9
H12H <sucodetr@protonmail.com>
2024-05-19 20:50:32 +0530

    added the text prompt

--- src/composer.rs
+++ src/composer.rs
@@ -3,31 +3,50 @@ use std::io::{self, stdout};
 
 use crossterm::{
     event::{self, Event, KeyCode},
+    queue,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    ExecutableCommand,
 };
 use ratatui::{prelude::*, widgets::*};
 
 pub fn main() -> io::Result<()> {
+    queue!(stdout(), EnterAlternateScreen)?;
     enable_raw_mode()?;
-    stdout().execute(EnterAlternateScreen)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+    let mut prompt_string = String::from("█");
 
     let mut should_quit = false;
     while !should_quit {
-        terminal.draw(ui)?;
-        should_quit = handle_events()?;
+        should_quit = handle_events(&mut prompt_string)?;
+        terminal.draw(|frame| layout(frame, &prompt_string))?;
     }
 
     disable_raw_mode()?;
-    stdout().execute(LeaveAlternateScreen)?;
+    queue!(stdout(), LeaveAlternateScreen)?;
+
     Ok(())
 }
-fn handle_events() -> io::Result<bool> {
-    if event::poll(std::time::Duration::from_millis(50))? {
+fn handle_events(text: &mut String) -> io::Result<bool> {
+    if event::poll(std::time::Duration::from_millis(500))? {
         if let Event::Key(key) = event::read()? {
-            if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(true);
+            if key.kind == event::KeyEventKind::Press {
+                if KeyCode::Enter == key.code || KeyCode::Esc == key.code {
+                    return Ok(true);
+                } else {
+                    if let KeyCode::Char(x) = key.code {
+                        let cursor = text.pop().unwrap();
+                        text.push(x);
+                        text.push(cursor);
+                        return Ok(false);
+                    } else if let KeyCode::Backspace = key.code {
+                        let cursor = text.pop().unwrap();
+                        let _ = text.pop();
+                        text.push(cursor);
+                        return Ok(false);
+                    } else {
+                        dbg!(key.code);
+                        return Ok(true);
+                    }
+                }
             }
         }
     }
@@ -35,7 +54,7 @@ fn handle_events() -> io::Result<bool> {
 }
 
 // the main frame
-fn ui(frame: &mut Frame) {
+fn layout(frame: &mut Frame, prompt: &str) {
     let main_layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints(vec![Constraint::Percentage(12), Constraint::Percentage(88)])
@@ -45,7 +64,7 @@ fn ui(frame: &mut Frame) {
         .constraints(vec![Constraint::Percentage(35), Constraint::Percentage(65)])
         .split(main_layout[1]);
     let prompt =
-        Paragraph::new("prompt").block(Block::default().title("prompt").borders(Borders::ALL));
+        Paragraph::new(prompt).block(Block::default().title("prompt").borders(Borders::ALL));
     let preview =
         Paragraph::new("Preview").block(Block::default().title("Preview").borders(Borders::ALL));
     let buffer =
