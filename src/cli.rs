c0def8143b15e68e89da3d8e1591834f7e1066a4
H12H <sucodetr@protonmail.com>
2024-05-24 18:18:34 +0530

    the preview pane is almost ready the rest of the code needs refactoring before I go any further

---
+++ src/cli.rs
@@ -0,0 +1,56 @@
+
+use clap::{arg, ArgMatches, Command};
+
+#[derive(Debug)]
+pub enum ActionForCore {
+    // TODO: rename the enum
+    /// Will show top 8 or 10 or 16 buffers
+    Show,
+    /// Will paste the content of specified buffer
+    Paste(String),
+    /// Will copy the given string to the db
+    Copy(String),
+    /// compose action will only handle the compostion in
+    /// iteractive manner and will push it resultant to the
+    /// top or on stdout depending to command
+    Compose,
+}
+
+pub fn argument() -> ActionForCore {
+    let collect_argument = |buffers: &ArgMatches| {
+        let buf = buffers.get_raw("BUFFERS").unwrap();
+        buf.fold(String::new(), |mut arg, x| {
+            arg.push_str(x.to_str().expect("Convertion failed"));
+            arg.push(' ');
+            arg
+        })
+    };
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("show", _)) => ActionForCore::Show,
+        Some(("compose", _)) => ActionForCore::Compose,
+        Some(("copy", buffers)) => {
+            let parsed_arg = collect_argument(buffers);
+            ActionForCore::Copy(parsed_arg)
+        }
+        Some(("paste", buffers)) => {
+            let parsed_arg = collect_argument(buffers);
+            ActionForCore::Paste(parsed_arg)
+        }
+        _ => unreachable!(),
+    }
+}
+
+fn cli() -> Command {
+    Command::new("sb")
+        .about("New kind of clipboard")
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("paste").about("Paste text from buffer").arg(
+                arg!(<BUFFERS> ... "series of buffer").value_parser(clap::value_parser!(String)),
+            ),
+        )
+        .subcommand(Command::new("copy").about("Copy text to buffer"))
+        .subcommand(Command::new("show").about("Shows the recent buffers"))
+        .subcommand(Command::new("compose").about("Compose existing buffer to make a new paste"))
+}
